e04d1863f04f8617ce5ffc9db17eabd663fb3e4b
Kieran Sweeden <kieransweeden@gmail.com>
2024-08-11 18:44:42 +0100

    rename handlers

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func getSnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
 		http.NotFound(w, r)
@@ -22,16 +22,21 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(msg))
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func getSnippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func postSnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("GET /snippet/view/{id}", getSnippetView)
+	mux.HandleFunc("GET /snippet/create", getSnippetCreate)
+	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
 
 	log.Print("starting server on :4000")
 
