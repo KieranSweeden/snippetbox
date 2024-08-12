1455f6c07726f72021a4808a3310becf8d101fc0
Kieran Sweeden <kieransweeden@gmail.com>
2024-08-12 09:02:32 +0100

    seperate routes into seperate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,8 +13,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
-	flag.Parse() // parses flags set above, default values will be used if not called
+	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
@@ -22,24 +21,11 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// strip /static from incoming requests that match this route
-	// so file searches within the subtree have the correct path
-	// i.e. we don't want to find with a path starting with /static
-	// whilst the current directory is /static
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe().
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
