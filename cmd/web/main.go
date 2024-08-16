a34a986e9930078314d07dfeda3485d6368a115e
Kieran Sweeden <kieransweeden@gmail.com>
2024-08-16 21:19:03 +0100

    use form decoder helper in create snippet handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,7 @@ import (
 
 	"snippetbox.kieransweeden.dev/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
@@ -19,6 +20,7 @@ type application struct {
 	logger        *slog.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -49,10 +51,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		logger:        logger,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	logger.Info("starting server", "addr", *addr)
