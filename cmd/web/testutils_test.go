86261553466b68c410cc45e7853cd30895d04f41
Kieran Sweeden <kieransweeden@gmail.com>
2024-08-18 13:20:57 +0100

    add get testServer method

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log/slog"
 	"net/http"
@@ -41,4 +42,20 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 	}
 
 	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
 }
